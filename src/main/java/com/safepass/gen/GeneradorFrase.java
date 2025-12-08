cc58af1203972ea8f065498205b202c6c4682f97
ALFREDYTX <alfredhacker1409@gmail.com>
2025-12-07 22:26:17 -0600

    Eliminar comentarios innecesarios en la clase GeneradorFrase para mejorar la claridad del código

--- src/main/java/com/safepass/gen/GeneradorFrase.java
+++ src/main/java/com/safepass/gen/GeneradorFrase.java
@@ -20,7 +20,6 @@ public class GeneradorFrase {
         
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < numeroPalabras; i++) {
-            // Usamos palabras de diferentes dominios para variedad
             String palabra;
             int tipo = faker.random().nextInt(3);
             switch (tipo) {
@@ -35,7 +34,6 @@ public class GeneradorFrase {
                     break;
             }
             
-            // Limpiar la palabra (quitar espacios si es compuesta, acentos opcionalmente)
             palabra = palabra.replaceAll("\\s+", "").toLowerCase();
             
             sb.append(palabra);
